b60685240326ea5b2be9c32f702d8a072003ff52
Francisco-Cassio <franciscodecassiomourao@gmail.com>
2026-06-11 00:54:14 -0300

    :seedling: chore: adicionando alguns dados inicais

---
+++ dados_iniciais.sql
@@ -0,0 +1,21 @@
+insert into forma_pagamento (nome) values 
+('pix'),
+('cartão de crédito'),
+('cartão de débito'),
+('dinheiro');
+
+insert into consultorio (num, andar) values 
+(101, 1),
+(102, 1),
+(201, 2),
+(202, 2);
+
+insert into especialidade (nome_especialidade, valor_consulta) values 
+('clínico geral', 150.00),
+('cardiologia', 250.00),
+('pediatria', 200.00),
+('ortopedia', 220.00);
+
+insert into endereco (num_casa, rua, bairro, cidade, estado) values
+(123, 'rua das flores', 'centro', 'timon', 'ma'),
+(456, 'avenida piauí', 'são benedito', 'timon', 'ma');
